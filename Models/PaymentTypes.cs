cbd3f3582dcf735208c32db1f42635a270e296f7
Shubhangi Kandhare <shubh.kandhare@gmail.com>
2020-08-20 18:12:23 +0530

    Add PaymentType Class.

---
+++ Models/PaymentTypes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderApplication.Models
+{
+    public class PaymentTypes
+    {
+        public int Id { get; set; }
+        public string PaymentType { get; set; }
+    }
+
+
+}
